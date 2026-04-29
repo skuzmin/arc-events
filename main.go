7d501ffe2bc42e5788f97ad9f299fdc4423c5002
Serhii Kuzmin <erngorn@gmail.com>
2026-04-29 18:18:20 +0300

    add graceful shutdown

--- main.go
+++ main.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"arc-events/handlers"
-	"fmt"
+	"context"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	handlers.ArcEventsHandler()
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /arc-events", handlers.GetArcEvents)
-	fmt.Println("Server is running on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", handlers.CorsMiddleware(handlers.GzipMiddleware(mux))))
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: handlers.CorsMiddleware(handlers.GzipMiddleware(mux)),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+	log.Println("Server is running on localhost:8080")
+
+	<-ctx.Done()
+	log.Println("Stutting down...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Println("Shutdown error: ", err)
+	}
+	log.Println("Done")
 }
